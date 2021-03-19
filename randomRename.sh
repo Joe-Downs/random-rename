6614ab0b2a5f62adac56087dab79fb0631a283cc
Joe Downs <joedowns502@gmail.com>
2021-03-19 18:43:30 -0400

    randomRename.sh: add variable to set string length
    The length of the string returned by randomString.py can now be set
    by a variable within the shell script.

--- randomRename.sh
+++ randomRename.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# Change this variable to alter the random string length
+stringLength=10
+
 # The first argument is the file to be randomly named. The second is the
 # directory it is in
 file=$1
 directory=$2
 
 extension=`python3 ~/git/random-rename/getExtension.py $file`
-randomString=`python3 ~/git/random-rename/randomString.py`
+randomString=`python3 ~/git/random-rename/randomString.py $stringLength`
 
 # Uncomment for testing purposes
 #`konsole --hold -e echo $file $directory$randomString$extension`
