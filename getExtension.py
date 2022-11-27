1bd2b978250267f2ed80b003fc60d936e5f1c0df
Joe Downs <joe@dwns.dev>
2022-11-27 17:02:54 -0500

    handle hidden folders when getting extension

--- getExtension.py
+++ getExtension.py
@@ -4,7 +4,12 @@ import sys
 # the filename.
 filename = sys.argv[1]
 
-# Find the first period and take everything after it to be the extension. First
-# period in case there are multiple in the extension (e.g., foo.log.backup)
+# Extract the filename without the $PWD by slicing from the last "/" to the
+# end. (This is to prevent hidden folders from being part of the 'extensin.')
+# Then, find the first period and take everything after it to be the extension.
+# First period in case there are multiple in the extension (e.g.,
+# foo.log.backup
+# TODO: deal with hidden files
+filename = filename[filename.rfind("/"):]
 extension = filename[filename.find("."):]
 print(extension)
